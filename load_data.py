a222aa68575222ae998a1d0923c4eb3f13a03c6c
Christina Reissel <christina.reissel@cern.ch>
2025-08-14 12:12:45 -0700

    fix setup in load_data

--- load_data.py
+++ load_data.py
@@ -1,38 +1,36 @@
 from gwpy.timeseries import TimeSeries, TimeSeriesDict
-from pathlib import Path
 import yaml
+from utils import load_config
+from pathlib import Path
 
-with open('config.yaml', 'r') as f:
-    config = yaml.safe_load(f)
+def load_data(config, data_dir: str):
 
-# Point this to whatever directory you want to house
-# all of the data products this notebook creates
-data_dir = Path("./data")
+    background_dir = data_dir / "background_data"
+    background_dir.mkdir(parents=True, exist_ok=True)
 
-# And this to the directory where you want to download the data
-background_dir = data_dir / "background_data"
-background_dir.mkdir(parents=True, exist_ok=True)
+    segments = [
+        (1240579783, 1240587612), 
+        (1240594562, 1240606748), 
+        (1240624412, 1240644412),
+        (1240644412, 1240654372),
+        (1240658942, 1240668052),
+    ]
 
-# These are the GPS time of the start and end of the segments.
-# There's no particular reason for these times, other than that they
-# contain analysis-ready data
-segments = [
-    (1240579783, 1240587612), 
-    (1240594562, 1240606748), 
-    (1240624412, 1240644412),
-    (1240644412, 1240654372),
-    (1240658942, 1240668052),
-]
+    for (start, end) in segments:
+        # Download the data from GWOSC. This will take a few minutes.
+        duration = end - start
+        fname = background_dir / f"background-{start}-{duration}.hdf5"
+        if fname.exists():
+            continue
 
-for (start, end) in segments:
-    # Download the data from GWOSC. This will take a few minutes.
-    duration = end - start
-    fname = background_dir / f"background-{start}-{duration}.hdf5"
-    if fname.exists():
-        continue
+        ts_dict = TimeSeriesDict()
+        for ifo in config.general.ifos:
+            ts_dict[ifo] = TimeSeries.fetch_open_data(ifo, start, end, cache=True)
+        ts_dict = ts_dict.resample(config.general.sample_rate)
+        ts_dict.write(fname, format="hdf5")
 
-    ts_dict = TimeSeriesDict()
-    for ifo in config['general']['ifos']:
-        ts_dict[ifo] = TimeSeries.fetch_open_data(ifo, start, end, cache=True)
-    ts_dict = ts_dict.resample(config['general']['sample_rate'])
-    ts_dict.write(fname, format="hdf5")
+if __name__ == "__main__":
+
+    config = load_config(config_path='config.yaml')
+    data_dir = Path("./data")
+    load_data(config, data_dir)
